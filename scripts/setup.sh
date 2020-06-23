7509205d18dd0938707695fc3ce4b6ea75736a99
Tim Vergenz <vergenzt@gmail.com>
2020-06-23 07:10:00 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,4 +12,5 @@ gpaste \
 
 chmod 600 ../state/__SECRET__/players-with-roles.txt
 
-join ../state/{public/players,__SECRET__/players-with-roles}.txt
+join ../state/{public/players,__SECRET__/players-with-roles}.txt \
+  | for
