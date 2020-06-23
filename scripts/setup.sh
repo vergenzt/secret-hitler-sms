59e9cb7f208b8b53ba67ee0c955ebd28607fdb40
Tim Vergenz <vergenzt@gmail.com>
2020-06-23 07:02:24 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 gpaste \
-  ../state/public/players.txt \
+  <(awk '{print $1}' ../state/public/players.txt) \
   <( cat ../assets/player-slots.txt \
     | head -n $(wc -l ../state/public/players.txt | awk '{ print $1 }') \
     | gshuf \
