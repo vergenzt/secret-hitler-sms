61caf0f68a0b65327c40c5c5cbd3f423305aa4e6
Tim Vergenz <vergenzt@gmail.com>
2020-06-23 06:46:07 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@
 cd $(basename $0)
 
 cat ../assets/player-slots.txt \
-  | head -n $(wc -l ../state/public/players.txt)
-  | gshuf
+  | head -n $(wc -l ../state/public/players.txt) \
+  | gshuf \
   | paste -s -d ' ' ../state/public/players.txt -
