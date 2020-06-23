93acc85ffa60a5015ba20bb116c6de4435eef1b8
Tim Vergenz <vergenzt@gmail.com>
2020-06-23 02:36:16 -0400

    Updated _build.sh.

--- assets/_build.sh
+++ assets/_build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-SOURCE_PDF=Secret_Hitler_PnP_color_with_backface.pdf
 
 format-card() {
   PAGENUM=$1
@@ -21,19 +20,4 @@ format-card 0 4x2@ 0,2-3 role-fascist.png
 format-board() {
   PAGENUM=$1
   FILENAME=$2
-  convert \
-    $SOURCE_PDF[$PAGENUM] \
-    -trim \
-    -shave 3 \
-    -crop 2x1@ \
-    \( -clone -2 -rotate -90 \) \
-    \( -clone -2 -rotate  90 \) \
-    -delete 0-1 \
-    +append \
-    $FILENAME
 }
-
-format-board 14 board-fascist-5or6.png
-format-board 15 board-fascist-7or8.png
-format-board 16 board-fascist-9or10.png
-format-board 17 board-liberal.png
