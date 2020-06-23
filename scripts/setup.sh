502d178b69479617cbd7d75a84c0b58bb21f608c
Tim Vergenz <vergenzt@gmail.com>
2020-06-23 07:26:40 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,7 +12,7 @@ gpaste \
 
 chmod 600 ../state/__SECRET__/players.txt
 
-cat ../state/__SECRET__/players.txt \
+jq -rR ../state/__SECRET__/players.txt \
   | awk '{print }'
   # ...
   | xargs -n1 twilio --to=... --body=... --media-url=...
