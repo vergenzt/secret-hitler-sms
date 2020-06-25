6768a10cd751ca18e2c893f395aaa8f3f3505c4a
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 20:38:47 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,8 @@ cd "$(dirname "$0")"
 
 # assign player roles
 gpaste \
-  <( cat ../state/public/players-init.txt | awk '{print $1}' ) \
+  <( cat ../state/public/players-init.txt \
+    | awk '{print $1}' ) \
   <( cat ../assets/player-slots.txt \
     | head -n $(wc -l ../state/public/players.txt) \
     | gshuf \
