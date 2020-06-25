fff517b1e8a9c09955b6e3b924ac6f8ad85060b6
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 20:53:56 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,7 @@ PHONES=`awk '{print $2}' $PUBLIC/players-init.txt`
 
 NUM_PLAYERS=`cat $PUBLIC/players-init.txt | wc -l`
 ACTIVE_ROLES=`head -n $NUM_PLAYERS $PUBLIC/players-init.txt`
+PLAYER_ROLES=`gpaste <(echo "$PNAMES") <(echo "$ACTIVE_ROLES")`
 
 # assign player roles
 gpaste \
