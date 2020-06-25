32dd8b4d81bce6ad047eed33e73487803a188307
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 21:21:54 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,8 @@ ASSETS=../assets
 SECRET=../state/__SECRET__
 PUBLIC=../state/public
 
-test ! -f $SECRET/player-roles.txt || ( \
-  echo "$SECRET/player-roles.txt already exists. Is a game in progress?" \
+if [[ -f $SECRET/player-roles.txt ]]
+  echo "$SECRET/player-roles.txt already exists. Is a game in progress?"
 
 PNAMES=`awk '{print $1}' $PUBLIC/players-init.txt`
 PHONES=`awk '{print $2}' $PUBLIC/players-init.txt`
