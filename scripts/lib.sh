54e8b8a3d2a881a8234d7a6d9f19e4eaf1ce8faf
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 02:15:14 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -53,7 +53,7 @@ assign_player_roles() {
 
 ensure_drawable_policy_deck() {
   if [[ "$SECRET_POLICY_DECK_LENGTH" -lt 3 ]]; then
-    echo "$SECRET_POLICY_DECK_LENGTH policies remaining. Re-shuffling policy deck"
+    echo "$SECRET_POLICY_DECK_LENGTH policies remaining; re-shuffling policy deck."
     cat "$F_PUBLIC_POLICIES_AVAILABLE" | gshuf > $F_SECRET_POLICY_DECK
   fi
 }
