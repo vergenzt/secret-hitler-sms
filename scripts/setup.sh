215891d96b43530a41def59b6cf8919e7b73ebe5
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 20:52:14 -0400

    Updated .gitignore.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,10 +5,6 @@ cd "$(dirname "$0")"
 SECRET=../state/__SECRET__
 PUBLIC=../state/public
 
-mkdir -p $SECRET $PUBLIC
-sudo chown root $SECRET
-sudo chmod 600 $SECRET
-
 PNAMES=`awk '{print $1}' $PUBLIC/players-init.txt`
 PHONES=`awk '{print $2}' $PUBLIC/players-init.txt`
 
