3657950b0f6b34f8e0881efe31c5d9b24d4bdcf4
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 01:38:31 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -36,10 +36,6 @@ assign_player_roles() {
   echo "$SECRET_PLAYER_ROLES" > $F_SECRET_PLAYER_ROLES
 }
 
-NUM_PLAYERS=`cat $PUBLIC/players-init.txt | wc -l`
-ACTIVE_ROLES=`head -n $NUM_PLAYERS $STATIC/roles-available.txt`
-PLAYER_ROLES=`gpaste <(echo $PNAMES) <(echo "$ACTIVE_ROLES" | gshuf)`
-
 
 # send texts
 while read PNAME PHONE ROLE PARTY; do
