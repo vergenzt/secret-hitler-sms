99ecb42486cb8489ac4071ea1ab840f4f4ddd5db
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 06:52:07 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -127,7 +127,7 @@ legislate() {
 
   ensure_drawable_policy_deck
 
-  read SECRET_POLICIES=`tail -n3 "$F_SECRET_POLICY_DECK" | tr '\n' '-'`
+  SECRET_POLICIES=`tail -n3 "$F_SECRET_POLICY_DECK" | tr '\n' '-'`
   send_sms "$PUBLIC_PRESIDENT_PHONE" <<EOF
     Congratulations on the election, $PUBLIC_PRESIDENT_PREFIX $PUBLIC_PRESIDENT_NAME.
     Here are your policy choices. Reply to choose one.
