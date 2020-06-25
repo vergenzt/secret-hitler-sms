018de58f5b662d1c499cb44dab1f2ecfc2038973
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 21:02:50 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,7 +14,7 @@ ACTIVE_ROLES=`head -n $NUM_PLAYERS $ASSETS/roles-available.txt`
 PLAYER_ROLES=`gpaste <(echo "$PNAMES") <(echo "$ACTIVE_ROLES" | gshuf)`
 
 # save roles
-gpaste <(echo "$PNAMES") <(echo "$PLAYER_ROLES")` > $SECRET/player-roles.txt
+gpaste <(echo "$PNAMES") <(echo "$PLAYER_ROLES") > $SECRET/player-roles.txt
 
 # send texts
 #while read PNAME PHONE ROLE; do
