f5d4c09ae238173a6147b5fcf90a773b21c30aa9
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 20:59:45 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,4 +13,5 @@ NUM_PLAYERS=`cat $PUBLIC/players-init.txt | wc -l`
 ACTIVE_ROLES=`head -n $NUM_PLAYERS $ASSETS/roles-available.txt`
 PLAYER_ROLES=`gpaste <(echo "$PNAMES") <(echo "$ACTIVE_ROLES" | gshuf)`
 
+# save roles
 gpaste <(echo "$PNAMES") <(echo "$PLAYER_ROLES")` > $SECRET/player-roles.txt
