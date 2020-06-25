f6e5f3439c0b330952ad618c4b86fc84de5b3a9d
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 21:53:22 -0400

    Updated legislate.sh.

---
+++ scripts/legislate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+set -x
+
+SOURCE_PHONE=+19044789601
+IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/assets/images
+ASSETS=../assets
+SECRET=../state/__SECRET__
+PUBLIC=../state/public
