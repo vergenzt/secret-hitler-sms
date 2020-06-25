ebd7769d9647fd0bc784907ebbd52f4e53168f9d
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 01:21:48 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,2 +1,10 @@
 
-ensure_drawable_policy_deck.sh
+IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/assets/images
+ASSETS=../assets
+SECRET=../state/__SECRET__
+PUBLIC=../state/public
+SOURCE_PHONE=`cat $STATE/source-phone.txt`
+
+ensure_drawable_policy_deck() {
+
+}
