f82e41bdafb8f79e6bdd0c0f4219b73c13cfd14d
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 06:28:33 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -51,8 +51,8 @@ start_sms_reply_listener() {
 
 await_sms_reply() {
   echo -n "Listening for SMS reply... "
-  nc -l localhost 8080 < $STATIC/twilio-empty-response.xml | \
-    node -r http -r -e '
+  nc -l localhost 8080 < $STATIC/twilio-empty-response.xml \
+    | node -r http -r gavel -e '
 
     '
   PARSED_RESP=`echo "$RESP" | node '
