acbe5b752f205109f538fa7b0050e0237afaa1d8
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 05:14:20 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -35,10 +35,11 @@ await_sms_reply() {
   echo -n "Starting ngrok server... "
   F_SECRET_NGROK_LOG=$SECRET/ngrok.json
   (ngrok http --log=stdout --log-format=json 8080 > $F_SECRET_NGROK_LOG &) 2>/dev/null
-  ngrok http --log=stdout --log-format=json 8080
-    | tail -n+0 -f $F_SECRET_NGROK_LOG \
+  SECRET_NGROK_URL=$(
+    tail -n+0 -f $F_SECRET_NGROK_LOG \
       | grep -q '"started tunnel".*"command line (http)"'
       | jq .url
+  )
   echo "Done."
   echo -n "Updating Twilio callback URL... "
   twilio phone-numbers:update $PUBLIC_SOURCE_PHONE --sms-url=$SECRET_NGROK_URL
