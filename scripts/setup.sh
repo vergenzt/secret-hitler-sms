917b7b970b863644f2f8060b700776c61f4975f2
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 20:44:38 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,11 @@
 set -x
 cd "$(dirname "$0")"
 
-STATE_S=../state/__SECRET__
-STATE_P=../state/public
+SECRET=../state/__SECRET__
+PUBLIC=../state/public
 sudo chown $STATE_S && sudo chmod 600 $STATE_S
 
-F_PLAYERS_INIT=../state/public/players-init.txt
+F_PLAYERS_INIT=$STATE_PUB
 F_PLAYER_ROLES=../state/__SECRET__/player-roles.txt
 
 PLAYERS=awk '{print $1}'
