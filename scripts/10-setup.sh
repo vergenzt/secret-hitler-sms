0e804a41f159b251f6dbcb9856f853715a1c10c2
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 01:24:14 -0400

    Updated 10-setup.sh.

--- scripts/10-setup.sh
+++ scripts/10-setup.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
-set -x
-
-IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/assets/images
-ASSETS=../assets
-SECRET=../state/__SECRET__
-PUBLIC=../state/public
-SOURCE_PHONE=`cat $STATE/source-phone.txt`
+source lib.sh
 
 if [[ -f $SECRET_PLAYER_ROLES_F ]]; then
   echo "Error: $SECRET_PLAYER_ROLES_F already exists."
