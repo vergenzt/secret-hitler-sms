b1f36bfd91071886314f5c90e105af8ebf196bf8
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 03:21:21 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -18,7 +18,7 @@ send_sms() {
     --from "$PUBLIC_SOURCE_PHONE" \
     --to "$PUBLIC_PHONE" \
     --body "" \
-    "$(printf "--media-url %s" "$@")"
+    ${ARGS[@]/#/--media-url }
 }
 
 F_PUBLIC_SOURCE_PHONE=$PUBLIC/source-phone.txt
