e3c72e4ee22efb7234abe77a8c59546296c327fb
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 07:10:12 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -132,7 +132,7 @@ legislate() {
     Congratulations on the election, $PUBLIC_PRESIDENT_PREFIX $PUBLIC_PRESIDENT_NAME. Here are your policy choices. Please reply:
     1) to discard the left ${SECRET_POLICIES[0]} policy and pass the others (${SECRET_POLICIES[1]} and ${SECRET_POLICIES[2]}) to $PUBLIC_CHANCELLOR_NAME.
 EOF
-  PRESIDENT_IMAGE=`image_url policycombo $(IFS="-"; echo ${SECRET_POLICIES[*]})`
+  PRESIDENT_IMAGE=`image_url policycombo $(IFS="-"; echo "${SECRET_POLICIES[*]}")`
   echo send_sms "$PUBLIC_PRESIDENT_PHONE" "$PRESIDENT_MSG" "$PRESIDENT_IMAGE"
 
   # send to president
