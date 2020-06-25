6c169c9fbed02507476912e5a6ba92422daebf6e
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 21:37:40 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,8 @@ SECRET=../state/__SECRET__
 PUBLIC=../state/public
 
 if [[ -f $SECRET/player-roles.txt ]]; then
-  echo "Error: $SECRET/player-roles.txt already exists. Is a game in progress?"
+  echo "Error: $SECRET/player-roles.txt already exists.""
+  echo "Is a game in progress?"
   echo "Please delete it if you're sure you want to start a new game."
   exit 1
 fi
