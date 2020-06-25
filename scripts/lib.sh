3e936e7e0797fafddbe2950b904b90474bacb0f7
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 06:59:27 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -129,9 +129,8 @@ legislate() {
 
   tail -n3 "$F_SECRET_POLICY_DECK" | readarray -t SECRET_POLICIES
   read PRESIDENT_MSG <<EOF | sed 's/^ +| +$//g' | tr '\n' ' '
-    Congratulations on the election, $PUBLIC_PRESIDENT_PREFIX $PUBLIC_PRESIDENT_NAME.
-    Here are your policy choices. Reply to choose one.
-    Reply 1 to discard $()
+    Congratulations on the election, $PUBLIC_PRESIDENT_PREFIX $PUBLIC_PRESIDENT_NAME. Here are your policy choices. Please reply:
+    Reply 1 to discard $
 EOF
   send_sms "$PUBLIC_PRESIDENT_PHONE"
 
