7bb6b4f05d1122f209f877005966c7382e8c3990
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 01:52:56 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -47,7 +47,7 @@ assign_player_roles() {
       --from "$STATIC_SOURCE_PHONE" \
       --to "$PUBLIC_PHONE" \
       --body "Hi $PUBLIC_NAME! Here's your secret role and party membership cards for Secret Hitler. 🙂 Enjoy the game!" \
-      --media-url "$IMAGES_BASE_URL/party-$SECRET_PARTY.png" \
+      --media-url `party_image $SECRET_PARTY" \
       --media-url "$IMAGES_BASE_URL/role-$SECRET_ROLE.png"
   done < <(join $F_PUBLIC_PLAYER_INFO $F_SECRET_PLAYER_ROLES | tr ':' ' ')
 }
