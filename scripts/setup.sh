c2b908db1dd6531f0ed5a5a93cbc8c69332caae0
Tim Vergenz <vergenzt@gmail.com>
2020-06-24 20:45:22 -0400

    Updated setup.sh.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ PUBLIC=../state/public
 sudo chown $STATE_S && sudo chmod 600 $STATE_S
 
 PLAYERS=awk '{print $1}' $PUBLIC/players-init.txt
- PHONES=awk '{print $2}' ../state/public/players-init.txt
+PPHONES=awk '{print $2}' ../state/public/players-init.txt
 
 NUM_PLAYERS=wc -l ../state/public/players-init.txt
 
