c8b8b1e4976cab50e240b7f338a25c8e63e79086
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 04:00:34 -0400

    Updated lib.sh.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -91,7 +91,7 @@ legislate() {
   #- useful commands:
 
   F_SECRET_NGROK_LOG=$SECRET/ngrok.log
-  ngrok http --log=$F_SECRET_NGROK_LOG --log-format=json 80
+  ngrok http --log=stdout --log-format=json 80 > $F_SECRET_NGROK_LOG
   jq --unbuffered 'select()'
 
   #- parse ngrok url from log output
