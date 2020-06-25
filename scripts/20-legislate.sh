e049e3b1952b23436b6121a05914288ecab6cb41
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 01:21:57 -0400

    Updated 20-legislate.sh.

--- scripts/20-legislate.sh
+++ scripts/20-legislate.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 set -x
-
-IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/assets/images
-ASSETS=../assets
-SECRET=../state/__SECRET__
-PUBLIC=../state/public
-SOURCE_PHONE=`cat $STATE/source-phone.txt`
+source lib.sh
 
 # who's president?
 # who's chancellor?
