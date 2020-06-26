06c05ba5f49b620b5ec4e6226e30f48ee8e3f298
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 02:08:33 -0400

    Updated peek_policy_deck.sh.

--- scripts/peek_policy_deck.sh
+++ scripts/peek_policy_deck.sh
@@ -14,3 +14,4 @@ PEEKER_PHONE=$(lookup "$PUBLIC_PLAYER_PHONES" "$PUBLIC_PLAYER_NAMES" "$PEEKER")
 PEEKER_MSG="$PEEKER, here are the current top three policies on the policy deck."
 PEEKER_IMG=`image_url policycombo "$(head -n3 $SECRET/policy-deck.txt | tr -d '[[:digit:]]' | tr '\n' '-')"`
 send_sms "$PEEKER_PHONE" "$PEEKER_MSG" "$PEEKER_IMG"
+echo "Done."
