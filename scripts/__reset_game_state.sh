360119a6a2a5f647b700c0f874c8a8a878ab16d2
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 01:16:12 -0400

    Updated __reset_game_state.sh.

--- scripts/__reset_game_state.sh
+++ scripts/__reset_game_state.sh
@@ -1,5 +1,6 @@
-
 #!/usr/bin/env bash
 (return 0 2>/dev/null) || cd "$(dirname "$0")"/.. || exit 1
 trap "kill 0" EXIT
 source scripts/__lib.sh
+
+rm $SECRET/policy-{deck,options,discard,record}.txt
