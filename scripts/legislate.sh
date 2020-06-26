de0162a15748547afc3dd115f49924d1ee46c2e5
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 01:59:59 -0400

    Updated legislate.sh.

--- scripts/legislate.sh
+++ scripts/legislate.sh
@@ -131,7 +131,7 @@ legislate() {
     | xargs send_sms \{\} \
       "President $PUBLIC_PRESIDENT_NAME and Chancellor $PUBLIC_CHANCELLOR_NAME have passed a $PUBLIC_POLICY_PASSED policy." \
       "`image_url policy "$PUBLIC_POLICY_PASSED"`"
-      
+
   ensure_drawable_policy_deck
 }
 
