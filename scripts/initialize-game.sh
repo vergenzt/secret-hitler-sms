a8cbf60431fa03fdd993469925483ee7b06658cf
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 01:21:49 -0400

    Updated initialize-game.sh.

--- scripts/initialize-game.sh
+++ scripts/initialize-game.sh
@@ -13,8 +13,9 @@ fi
 echo -n "Assigning player roles... "
 SECRET_PLAYER_ROLES=`echo "$PUBLIC_ROLES_ACTIVE" | gshuf | tee $SECRET/player-roles.txt`
 echo "Done."
+
 echo -n "Initializing decks... "
-rm $SECRET/policy-{deck,options,discard,record}.txt
+touch $SECRET/policy-{deck,options,discard,record}.txt
 
 while read PUBLIC_NAME PUBLIC_PHONE SECRET_ROLE SECRET_PARTY; do
   send_sms \
