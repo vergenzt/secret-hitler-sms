73336e81a32dcbf89bd55d33fc997f53c21e4355
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 00:52:11 -0400

    Updated _lib.sh.

--- scripts/_lib.sh
+++ scripts/_lib.sh
@@ -77,5 +77,5 @@ move_card() {
 # pick a card from position $I of $FROM_DECk
 pick_card() {
   I=$1; FROM_DECK=$2
-  awk "NR == $I { print \$0 }" "$FROM_DECK"
+  awk "NR == $I { print \$0 }" "$FROM_DECK" | tr -d '[[:digit:]]'
 }
