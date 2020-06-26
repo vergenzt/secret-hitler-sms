230107769c9cbe44e6c919cf767976009d63dce6
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 02:02:34 -0400

    Updated peek_policy_deck.sh.

--- scripts/peek_policy_deck.sh
+++ scripts/peek_policy_deck.sh
@@ -2,5 +2,7 @@
 (return 0 2>/dev/null) || cd "$(dirname "$0")"/.. || exit 1
 source scripts/__lib.sh
 
-read -p "Who gets to peek at the policy deck?  ($PUBLIC_PLAYER_NAMES_PROMPT): " PEEKER
-read -p "Confirm: " PEEKER_CONFIRM
+while [[ "$PEEKER" != "$PEEKER_CONFIRM" ]]; do
+  read -p "Who gets to peek at the policy deck?  ($PUBLIC_PLAYER_NAMES_PROMPT): " PEEKER
+  read -p "Confirm: " PEEKER_CONFIRM
+done
