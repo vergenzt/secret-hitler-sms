bdcdcb29066a381cec4c68894226c9d05c1e400c
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 23:20:23 -0400

    Updated legislate.sh.

--- scripts/_lib.sh
+++ scripts/_lib.sh
@@ -67,8 +67,8 @@ draw_cards() {
   awk "NR >  $N { print $0 }" "$FROM_DECK" | sponge > "$FROM_DECK"
 }
 
-# remove 1 card from position $I of $FROM_DECK and append to tail of $TO_DECK
-remove_card() {
+# move 1 card from position $I of $FROM_DECK and append to tail of $TO_DECK
+move_card() {
   I=$1; FROM_DECK=$2; TO_DECK=$3
   awk "NR == $I { print $0 }" "$FROM_DECK" >> "$TO_DECK"
   awk "NR != $I { print $0 }" "$FROM_DECK" | sponge > "$FROM_DECK"
