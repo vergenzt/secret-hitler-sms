2c04c877da559d5ea8d5c72dd6f3a9836e6e19c8
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 01:52:55 -0400

    Updated send-party-membership-card.sh.

--- scripts/send-party-membership-card.sh
+++ scripts/send-party-membership-card.sh
@@ -2,4 +2,12 @@
 (return 0 2>/dev/null) || cd "$(dirname "$0")"/.. || exit 1
 source scripts/__lib.sh
 
+FROM="$1"
+
+echo "You are about to send "
+while [[ "$RESET" != "RESET" ]]; do
+  read -p "To reset game state, please type RESET: " RESET
+done
+rm -f $SECRET/{policy-{deck,options,discard},player-roles}.txt
+rm -f $PUBLIC/policies-enacted.txt
 send_sms "$(lookup "$PUBLIC_PLAYER_PHONES"
