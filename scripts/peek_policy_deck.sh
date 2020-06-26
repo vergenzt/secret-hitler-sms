40d2addafa209ddcb5795de1877892cd4164ea75
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 02:06:15 -0400

    Updated peek_policy_deck.sh.

--- scripts/peek_policy_deck.sh
+++ scripts/peek_policy_deck.sh
@@ -8,6 +8,7 @@ while [[ -z "$PEEKER" || "$PEEKER" != "$PEEKER_CONFIRM" ]]; do
 done
 
 ensure_drawable_policy_deck
+
 echo -n "Sending preview of top three policies to $PEEKER... "
 PEEKER_PHONE=$(lookup "$PUBLIC_PLAYER_PHONES" "$PUBLIC_PLAYER_NAMES" "$PEEKER")
 PEEKER_MSG="Here are the current top three policies on the policy deck."
