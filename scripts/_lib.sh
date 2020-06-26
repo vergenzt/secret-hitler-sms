56a54d714c2fef6b0030ec6a80073055e3de490b
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 20:25:57 -0400

    Updated _lib.sh.

--- scripts/_lib.sh
+++ scripts/_lib.sh
@@ -72,37 +72,6 @@ F_SECRET_POLICY_DISCARD=$SECRET/policy-discard.txt
 
 SECRET_POLICY_DECK_LENGTH=`cat $F_SECRET_POLICY_DECK 2>/dev/null | wc -l`
 
-assign_player_roles() {
-  if [[ -f $F_SECRET_PLAYER_ROLES ]]; then
-    echo "Error: $F_SECRET_PLAYER_ROLES already exists."
-    echo -e "Is a game in progress?\n"
-    echo "Please delete it if you're sure you want to start a new game."
-    exit 1
-  fi
-
-  # assign
-  echo "Assigning player roles!"
-  SECRET_PLAYER_ROLES=`gpaste <(echo "$PUBLIC_PLAYER_NAMES") <(echo "$PUBLIC_ROLES_ACTIVE" | gshuf)`
-
-  # save
-  echo "$SECRET_PLAYER_ROLES" > $F_SECRET_PLAYER_ROLES
-
-  # send texts
-  while read PUBLIC_NAME PUBLIC_PHONE SECRET_ROLE SECRET_PARTY; do
-    send_sms \
-      "$PUBLIC_PHONE" \
-      "Hi $PUBLIC_NAME! Here's your secret role and party membership cards for Secret Hitler. 🙂 Enjoy the game!" \
-      "`image_url party $SECRET_PARTY`" \
-      "`image_url role $SECRET_ROLE`"
-  done < <(join $F_PUBLIC_PLAYER_INFO $F_SECRET_PLAYER_ROLES | tr ',' ' ')
-}
-
-ensure_drawable_policy_deck() {
-  if [[ "$SECRET_POLICY_DECK_LENGTH" -lt 3 ]]; then
-    echo "$SECRET_POLICY_DECK_LENGTH policies remaining; re-shuffling policy deck."
-    cat "$F_PUBLIC_POLICIES_AVAILABLE" | gshuf > $F_SECRET_POLICY_DECK
-  fi
-}
 
 legislate() {
   # who's president?
