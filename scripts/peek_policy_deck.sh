64f6f6abe21b69fbb44a4e0d12c513ce57e6efe5
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 02:01:18 -0400

    Updated peek_policy_deck.sh.

--- scripts/peek_policy_deck.sh
+++ scripts/peek_policy_deck.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 (return 0 2>/dev/null) || cd "$(dirname "$0")"/.. || exit 1
 source scripts/__lib.sh
+
+read -p "Who's President?  ($PUBLIC_PLAYER_NAMES_PROMPT): " PUBLIC_PRESIDENT_NAME
+read -p "Who's Chancellor? ($PUBLIC_PLAYER_NAMES_PROMPT): " PUBLIC_CHANCELLOR_NAME
