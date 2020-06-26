33eb9c4230d196c20bc65f1dd8c94ddf71503761
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 00:45:46 -0400

    Updated _lib.sh.

--- scripts/_lib.sh
+++ scripts/_lib.sh
@@ -64,14 +64,14 @@ ensure_drawable_policy_deck() {
 draw_cards() {
   N=$1; FROM_DECK=$2; TO_DECK=$3
   cat "$FROM_DECK" | awk "NR <= $N { print \$0 }" >> "$TO_DECK"
-  cat "$FROM_DECK" | awk "NR >  $N { print \$0 }" | sponge > "$FROM_DECK"
+  cat "$FROM_DECK" | awk "NR >  $N { print \$0 }" | sponge "$FROM_DECK"
 }
 
 # move 1 card from position $I of $FROM_DECK and append to tail of $TO_DECK
 move_card() {
   I=$1; FROM_DECK=$2; TO_DECK=$3
   cat "$FROM_DECK" | awk "NR == $I { print \$0 }" >> "$TO_DECK"
-  cat "$FROM_DECK" | awk "NR != $I { print \$0 }" | sponge > "$FROM_DECK"
+  cat "$FROM_DECK" | awk "NR != $I { print \$0 }" | sponge "$FROM_DECK"
 }
 
 # pick a card from position $I of $FROM_DECk
