a086c5036889a818b387966929c25bd377d47297
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 23:35:18 -0400

    Updated legislate.sh.

--- scripts/legislate.sh
+++ scripts/legislate.sh
@@ -57,7 +57,6 @@ legislate() {
   ensure_drawable_policy_deck
 
   draw_cards 3 "$F_SECRET_POLICY_DECK" "$F_SECRET_POLICY_OPTIONS"
-  read P1 P2 P3 <(cat "$F_SECRET_POLICY_OPTIONS" | tr '\n' ' '; echo)
 
   echo -n "Sending policy options to President $PUBLIC_PRESIDENT_NAME... "
   PRESIDENT_MSG=$(
