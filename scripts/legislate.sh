cfa6aa72cbc34973d5220ad100bf82389299ca27
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 23:04:02 -0400

    Updated legislate.sh.

--- scripts/legislate.sh
+++ scripts/legislate.sh
@@ -61,7 +61,7 @@ legislate() {
     "Reply 2 to discard the middle ${SECRET_POLICIES[1]} policy and pass ${SECRET_POLICIES[0]}-${SECRET_POLICIES[2]} to Chancellor $PUBLIC_CHANCELLOR_NAME.\n\n"\
     "Reply 3 to discard the right ${SECRET_POLICIES[2]} policy and pass ${SECRET_POLICIES[0]}-${SECRET_POLICIES[1]} to Chancellor $PUBLIC_CHANCELLOR_NAME.\n\n"\
   )
-  PRESIDENT_IMAGE=`image_url policycombo $(IFS="-"; echo "${SECRET_POLICIES[*]}")`
+  PRESIDENT_IMAGE=`image_url policycombo "$(IFS="-"; echo "${SECRET_POLICIES[*]}")"`
   send_sms "$PUBLIC_PRESIDENT_PHONE" "$PRESIDENT_MSG" "$PRESIDENT_IMAGE"
   echo "Sent. Awaiting response."
 
