b28dc7da59795a89faf8fc7fe04c245373d60243
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 00:22:25 -0400

    Updated _lib.sh.

--- scripts/_lib.sh
+++ scripts/_lib.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 lookup() {
-  gpaste <(echo "$2") <(echo "$1") # | awk "\$1 == \"$3\" { print \$2 }"
+  gpaste <(echo "$2") <(echo "$1") | awk "\$1 == \"$3\" { print \$2 }"
 }
 
 # https://stackoverflow.com/a/37840948
