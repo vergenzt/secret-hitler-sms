f30773fd054d0173f7dd9c576a2c228e876dba8c
Tim Vergenz <vergenzt@gmail.com>
2020-06-25 21:34:11 -0400

    Updated legislate.sh.

--- scripts/legislate.sh
+++ scripts/legislate.sh
@@ -22,7 +22,7 @@ await_sms_reply_from() {
     return 1
   fi
   while true; do
-    TWILIO_RESP=`nc -l localhost 8080 < $STATIC/twilio-empty-response.xml | tail -n1 | tr '&' '\n'`
+    TWILIO_RESP=`nc -l localhost 8080 < $STATIC/twilio-empty-response.xml | tail -n1 | tr '&=' '\n '`
     echo "$TWILIO_RESP"
     #grep -q "&From=$(echo "$1" | tr '+' '%2B')&" <(echo "$TWILIO_RESP")
   done
