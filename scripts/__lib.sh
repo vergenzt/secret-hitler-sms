b90fb1add3999a2a102a1adab4563ce7b8a49e1c
Tim Vergenz <vergenzt@gmail.com>
2020-06-26 20:43:01 -0400

    Updated __lib.sh.

--- scripts/__lib.sh
+++ scripts/__lib.sh
@@ -56,7 +56,7 @@ policy_deck_length() {
 ensure_drawable_policy_deck() {
   if [[ `policy_deck_length` -lt 3 ]]; then
     echo "$(policy_deck_length) policies in deck; shuffling."
-    cat "$SECRET/policy-discard.txt" "$SECRET/policy-deck.txt" | gshuf > $SECRET/policy-deck.txt
+    cat "$SECRET/policy-discard.txt" "$SECRET/policy-deck.txt" | gshuf | sponge $SECRET/policy-deck.txt
   fi
 }
 
