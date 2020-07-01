568c7ccccae0a8149d31e1f455b9844622212193
Tim Vergenz <vergenzt@gmail.com>
2020-06-30 22:23:13 -0400

    Updated vars-live.sh.

--- scripts/lib/vars-live.sh
+++ scripts/lib/vars-live.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/$STATIC/images
-image_url() { echo "$IMAGES_BASE_URL/$1-$2.png"; }
 
-F_PUBLIC_SOURCE_PHONE=$PUBLIC/source-phone.txt
-F_PUBLIC_PLAYER_INFO=$PUBLIC/player-info.txt
-F_PUBLIC_ROLES_AVAILABLE=$STATIC/roles-available.txt
-F_PUBLIC_POLICIES_AVAILABLE=$STATIC/policies-available.txt
-
-PUBLIC_SOURCE_PHONE=`cat $F_PUBLIC_SOURCE_PHONE 2>/dev/null`
-PUBLIC_PLAYER_INFO=`cat $F_PUBLIC_PLAYER_INFO | grep -v '^#'`
-PUBLIC_PLAYER_TITLES=`awk '{print $1}' <(echo "$PUBLIC_PLAYER_INFO")`
-PUBLIC_PLAYER_NAMES=`awk '{print $2}' <(echo "$PUBLIC_PLAYER_INFO")`
-PUBLIC_PLAYER_PHONES=`awk '{print $3}' <(echo "$PUBLIC_PLAYER_INFO")`
-PUBLIC_PLAYER_NAMES_PROMPT=`echo "$PUBLIC_PLAYER_NAMES" | tr '\n' '/'`
-
-PUBLIC_NUM_PLAYERS=`echo "$PUBLIC_PLAYER_INFO" | wc -l`
-PUBLIC_ROLES_ACTIVE=`head -n $PUBLIC_NUM_PLAYERS $F_PUBLIC_ROLES_AVAILABLE`
-
-F_SECRET_PLAYER_ROLES=$SECRET/player-roles.txt
-F_SECRET_POLICY_DECK=$SECRET/policy-deck.txt
-F_SECRET_POLICY_OPTIONS=$SECRET/policy-options.txt
-F_SECRET_POLICY_DISCARD=$SECRET/policy-discard.txt
-
-F_SECRET_NGROK_LOG=$SECRET/ngrok.json
+SECRET=state/__SECRET__
+PUBLIC=state/public
