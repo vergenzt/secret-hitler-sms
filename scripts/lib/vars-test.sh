e81d3cfd531942cb38f6bc53366223ba3a3c4621
Tim Vergenz <vergenzt@gmail.com>
2020-06-30 22:20:44 -0400

    Updated vars-test.sh.

--- scripts/lib/vars-test.sh
+++ scripts/lib/vars-test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SECRET=state/__SECRET__
+PUBLIC=state/public
+
 IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/$STATIC/images
 image_url() { echo "$IMAGES_BASE_URL/$1-$2.png"; }
 
