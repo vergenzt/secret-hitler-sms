a422d36fb010a04fcf362edbc6da049c0b855008
Tim Vergenz <tim@truelinkfinancial.com>
2020-07-01 09:49:32 -0400

    Updated peek_policy_deck.

--- scripts/lib/deck.test.bats
+++ scripts/lib/deck.test.bats
@@ -15,4 +15,14 @@ load deck
 	EOF
 
 	ensure_drawable_policy_deck
+
+	diff -U1 "`sort $SECRET/policy-deck.txt`" <<-EOF
+	card1
+	card2
+	card3
+	card4
+	card5
+	EOF
+	diff -U1 "`sort $SECRET/policy-discard.txt`" <<-EOF
+	EOF
 }
--- scripts/lib/vars.bash
+++ scripts/lib/vars.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2155
+set -o errexit
 
-SECRET=state/__SECRET__
-PUBLIC=state/public
+export SECRET=state/__SECRET__
+export PUBLIC=state/public
 
 # set up temporary test state directories
 if [ ! -z "$BATS_TMPDIR" ]; then
@@ -9,28 +11,24 @@ if [ ! -z "$BATS_TMPDIR" ]; then
   PUBLIC=$BATS_TMPDIR/$PUBLIC
 fi
 
+mkdir -p "$SECRET"
+mkdir -p "$PUBLIC"
+
 STATIC=static
 IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/$STATIC/images
 image_url() { echo "$IMAGES_BASE_URL/$1-$2.png"; }
 
-F_PUBLIC_SOURCE_PHONE=$PUBLIC/source-phone.txt
-F_PUBLIC_PLAYER_INFO=$PUBLIC/player-info.txt
-F_PUBLIC_ROLES_AVAILABLE=$STATIC/roles-available.txt
-F_PUBLIC_POLICIES_AVAILABLE=$STATIC/policies-available.txt
+# check file presence
+[ -f "$PUBLIC/source-phone.txt" ]
+[ -f "$PUBLIC/player-info.txt" ]
+[ -f "$STATIC/roles-available.txt" ]
 
-PUBLIC_SOURCE_PHONE=`cat $F_PUBLIC_SOURCE_PHONE 2>/dev/null`
-PUBLIC_PLAYER_INFO=`cat $F_PUBLIC_PLAYER_INFO | grep -v '^#'`
-PUBLIC_PLAYER_TITLES=`awk '{print $1}' <(echo "$PUBLIC_PLAYER_INFO")`
-PUBLIC_PLAYER_NAMES=`awk '{print $2}' <(echo "$PUBLIC_PLAYER_INFO")`
-PUBLIC_PLAYER_PHONES=`awk '{print $3}' <(echo "$PUBLIC_PLAYER_INFO")`
-PUBLIC_PLAYER_NAMES_PROMPT=`echo "$PUBLIC_PLAYER_NAMES" | tr '\n' '/'`
+export PUBLIC_SOURCE_PHONE=`cat $PUBLIC/source-phone.txt 2>/dev/null`
+export PUBLIC_PLAYER_INFO=`cat $PUBLIC/player-info.txt | grep -v '^#'`
+export PUBLIC_PLAYER_TITLES=`awk '{print $1}' <(echo "$PUBLIC_PLAYER_INFO")`
+export PUBLIC_PLAYER_NAMES=`awk '{print $2}' <(echo "$PUBLIC_PLAYER_INFO")`
+export PUBLIC_PLAYER_PHONES=`awk '{print $3}' <(echo "$PUBLIC_PLAYER_INFO")`
+export PUBLIC_PLAYER_NAMES_PROMPT=`echo "$PUBLIC_PLAYER_NAMES" | tr '\n' '/'`
 
-PUBLIC_NUM_PLAYERS=`echo "$PUBLIC_PLAYER_INFO" | wc -l`
-PUBLIC_ROLES_ACTIVE=`head -n $PUBLIC_NUM_PLAYERS $F_PUBLIC_ROLES_AVAILABLE`
-
-F_SECRET_PLAYER_ROLES=$SECRET/player-roles.txt
-F_SECRET_POLICY_DECK=$SECRET/policy-deck.txt
-F_SECRET_POLICY_OPTIONS=$SECRET/policy-options.txt
-F_SECRET_POLICY_DISCARD=$SECRET/policy-discard.txt
-
-F_SECRET_NGROK_LOG=$SECRET/ngrok.json
+export PUBLIC_NUM_PLAYERS=`echo "$PUBLIC_PLAYER_INFO" | wc -l`
+export PUBLIC_ROLES_ACTIVE=`head -n $PUBLIC_NUM_PLAYERS $STATIC/roles-available.txt`
