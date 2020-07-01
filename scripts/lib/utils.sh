aea93355d2fb730bf9cb4777f7f7d122ececae8f
Tim Vergenz <vergenzt@gmail.com>
2020-06-30 21:40:22 -0400

    Updated vars.sh.

---
+++ scripts/lib/utils.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+lookup() {
+  gpaste <(echo "$2") <(echo "$1") | awk "\$1 == \"$3\" { print \$2 }"
+}
+
+# https://stackoverflow.com/a/37840948
+function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
