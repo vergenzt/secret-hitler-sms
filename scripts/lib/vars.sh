4a31be52ed5ef5143e2594441b4f7eedbb509699
Tim Vergenz <vergenzt@gmail.com>
2020-06-30 22:26:06 -0400

    Updated vars.sh.

--- scripts/lib/vars.sh
+++ scripts/lib/vars.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SECRET=$BATS_TMPDIR/state/__SECRET__
+PUBLIC=$BATS_TMPDIR/state/public
+
 STATIC=static
 IMAGES_BASE_URL=https://raw.githubusercontent.com/vergenzt/secret-hitler-sms/master/$STATIC/images
 image_url() { echo "$IMAGES_BASE_URL/$1-$2.png"; }
