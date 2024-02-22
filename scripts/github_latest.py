5eb1d4fc7127b98fddea377bcf7fc264b7792d1c
Tim Vergenz <vergenzt@gmail.com>
2024-02-22 00:30:21 -0500

    wip

--- scripts/github_latest.py
+++ scripts/github_latest.py
@@ -1,8 +1,15 @@
+import hashlib
 import json
 import subprocess as sp
+import textwrap
 
-def github_latest_url(repo: str, asset_name: str) -> str:
+def github_latest_url(pfx: str, repo: str, asset_name: str) -> str:
   version_url = f"https://api.github.com/repos/{repo}/releases/latest"
   version = json.loads(sp.check_output(["curl", "-sS", version_url], text=True))
   asset_url = f"https://github.com/{repo}/releases/download/{version['tag_name']}/{asset_name}"
-  return asset_url
+  asset_bytes = sp.check_output(["curl", "-sSL", asset_url])
+  asset_sha = hashlib.sha256(asset_bytes).hexdigest()
+  return textwrap.dedent(f"""
+    {pfx}_LATEST_URL={asset_url}
+    {pfx}_LATEST_SHA={asset_sha}
+  """).strip()
