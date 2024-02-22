ea44da5425e60ae09e59efd7df42d8ae59ed0d34
Tim Vergenz <vergenzt@gmail.com>
2024-02-22 01:09:05 -0500

    wip: update src/lib.sh

--- src/lib.sh
+++ src/lib.sh
@@ -5,7 +5,7 @@ if [ -z "${LIB_SOURCED:-}" ]; then
   LIB_SOURCED=true
 
   # https://stackoverflow.com/a/58098360
-  CODE_CHARS=({C,D,E,F,H,J,K,M,N,P,R,T,V,W,X,Y,2,3,4,5,6,8,9})
+  CODE_CHARS=(C D E F H J K M N P R T V W X Y 2 3 4 5 6 8 9)
   CODE_LEN=4
 
   STATIC=https://secret-h-sms.com
