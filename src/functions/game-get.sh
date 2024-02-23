1a46d82cb66758af2fe276ea617ae95876e2f469
Tim Vergenz <vergenzt@gmail.com>
2024-02-23 01:49:01 -0500

    change /game/{GAME_ID} to /game?id=

--- src/functions/game-get.sh
+++ src/functions/game-get.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 source src/lib.sh
 
-# httpApi: GET /game/{GAME_ID}
+# httpApi: GET /game
 
 function game-init() {
-  GAME_ID=$(echo "$1" | jq -e .pathParameters.GAME_ID)
+  GAME_ID=$(echo "$1" | jq -er .queryStringParameters.id)
 
   if ! aws s3 ls "$(printf "$GAME_INITIALIZED" "$GAME_ID")" >/dev/null; then
     RESP_STATUS=404
